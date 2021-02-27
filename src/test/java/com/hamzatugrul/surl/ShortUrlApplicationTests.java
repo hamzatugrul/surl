7448a8b5181b687ec606ff462a96b07877c59912
Hamza Tugrul Topcuoglu <hamzatugrultop@gmail.com>
2021-02-27 15:06:15 +0300

    Initial Commit of SURL Project Structure

---
+++ src/test/java/com/hamzatugrul/surl/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzatugrul.surl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
